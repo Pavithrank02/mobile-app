5a84bd90cb632171fa12e0039a9651c0396c22a3
Pavithran2 <pavithrank02@gmail.com>
2023-11-22 11:11:24 +0530

    components

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>App</Text>
-      <StatusBar style="auto" />
+      <Text>Mobile App</Text>
+      <Button title='tap'/>
     </View>
   );
 }
--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 
 
 # Learnings
+
 - Introduction
   - use expo to create app
   - learn about different components
   - no inline stylings
+
+- Working with components
+  - view components is like div in react and its a container
+  - button components needs to be imported unlike direct usage in react
